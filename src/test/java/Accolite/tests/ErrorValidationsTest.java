26a454fefd0e03f4e09c8788d073dc39e7e2de12
Vamsi-541 <krishnaaddankivamsi@gmail.com>
2024-07-25 17:55:12 +0530

    Framework Added

---
+++ src/test/java/Accolite/tests/ErrorValidationsTest.java
@@ -0,0 +1,51 @@
+package Accolite.tests;
+
+import org.testng.annotations.Test;
+
+import Accolite.TestComponents.BaseTest;
+import Accolite.TestComponents.Retry;
+import accolite.pageobjects.CartPage;
+import accolite.pageobjects.CheckoutPage;
+import accolite.pageobjects.ConfirmationPage;
+import accolite.pageobjects.ProductCatalogue;
+
+import org.testng.AssertJUnit;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+	
+		landingPage.loginApplication("anshika@gmail.com", "Iamki000");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("rahulshetty@gmail.com", "Iamking@000");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+		
+	
+
+	}
+
+	
+	
+
+}
